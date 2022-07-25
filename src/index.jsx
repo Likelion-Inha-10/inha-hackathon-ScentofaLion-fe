65bbd396caaebc948752ea4dda26318ddc1c761e
JunHyeon <dlwnsgus07@naver.com>
2022-07-26 00:15:15 +0900

    feat : add Store Provider

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,17 @@ import * as ReactDOMClient from 'react-dom/client';
 import Router from 'routes';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyle from 'styles/GlobalStyles';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <GlobalStyle />
-    <Router />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <GlobalStyle />
+      <Router />
+    </React.StrictMode>
+  </Provider>,
 );
 
 reportWebVitals();
