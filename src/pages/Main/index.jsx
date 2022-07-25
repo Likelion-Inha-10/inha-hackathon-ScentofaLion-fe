ca77a3aad6024cc43ad420d3864ce1745e99b371
JunHyeon <dlwnsgus07@naver.com>
2022-07-26 00:21:33 +0900

    fix : unused variables

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,12 +1,3 @@
-import { connect } from 'react-redux';
+const Main = () => <>test</>;
 
-const Main = (state) => <>{state.value}</>;
-
-const mapStateToProps = (state) => {
-  console.log(state.counter.step);
-  return {
-    value: state.counter.step,
-  };
-};
-
-export default connect(mapStateToProps)(Main);
+export default Main;
