4b977656151d533fe61bbc8a7be7d0e029026e12
miri <oo117mmm@gmail.com>
2022-07-28 17:28:33 +0900

    feat: fix review loading page

--- src/pages/ReviewLoading/index.jsx
+++ src/pages/ReviewLoading/index.jsx
@@ -5,6 +5,7 @@ import { animate } from 'components/Animation/animate';
 import Header from 'components/Header';
 import NavigationBar from 'components/NavigationBar';
 import Button from 'components/Button';
+import { useNavigate } from 'react-router-dom';
 
 const Text = styled.div`
   margin-top: 30px;
@@ -28,24 +29,9 @@ const MainText = styled.div`
   color: black;
 `;
 
-const Box = styled.div`
-  height: 400px;
-  width: 50%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  border-radius: 50px;
-  background-color: white;
-  box-shadow: 0 0 30px 1px rgba(0, 0, 0, 0.1);
-  padding-bottom: 30px;
-  margin-left: 8vw;
-  margin-right: ${(props) => props.margin || 0}vw;
-`;
-
 const BoxWrapper = styled.div`
   height: 600px;
-  width: 101%;
+  width: 100%;
   justify-content: center;
   margin-left: 10%;
   position: relative;
@@ -96,7 +82,21 @@ const BlackBox = styled.div`
   position: absolute;
 `;
 
+const BackWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
 const ReviewLoading = () => {
+  let navigate = useNavigate();
+
+  function moveToReviewPage() {
+    navigate('/home/review');
+  }
+  function moveToSampleChange() {
+    navigate('/home/edit');
+  }
+
   return (
     <motion.div
       initial={animate.initial}
@@ -113,8 +113,10 @@ const ReviewLoading = () => {
         새로운 경험을 원하신다면 <br />
         선택을 변경해보세요
       </MainText>
-      <WhiteBox></WhiteBox>
-      <BlackBox></BlackBox>
+      <BackWrapper>
+        <WhiteBox></WhiteBox>
+        <BlackBox></BlackBox>
+      </BackWrapper>
       <BoxWrapper>
         <Button
           marginTop="30"
@@ -125,12 +127,16 @@ const ReviewLoading = () => {
         >
           <EBoxText>Review</EBoxText>
           <BoxText>이번주 받아보신 제품은 어떠셨나요?</BoxText>
-          <BoxText fontWeight="900">리뷰 작성하러 가기</BoxText>
+          <BoxText fontWeight="900" onClick={moveToReviewPage}>
+            리뷰 작성하러 가기
+          </BoxText>
         </Button>
-        <Button width="80" height="35" radius="40">
+        <Button backgroundColor="black" width="80" height="35" radius="40">
           <EBoxText>New</EBoxText>
           <BoxText>다음주, 다른 카테고리의 제품을 받아보고 싶으신가요?</BoxText>
-          <BoxText fontWeight="900">샘플 변경하러 가기</BoxText>
+          <BoxText fontWeight="900" onclick={moveToSampleChange}>
+            샘플 변경하러 가기
+          </BoxText>
         </Button>
       </BoxWrapper>
 
